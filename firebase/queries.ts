981df863b17332df2e7fe4303734361f1ba3bf84
Eric Kim <bosung90@gmail.com>
2025-08-30 12:00:27 -0700

    make getPastEvents merge events from firestore with events.json

--- firebase/queries.ts
+++ firebase/queries.ts
@@ -4,14 +4,12 @@ import {firestore} from './app'
 import {Event} from './types'
 
 export async function getFutureEvents({
-  limit = 100,
   currentDate = new Date(),
 }: {limit?: number; currentDate?: Date} = {}) {
   return firestore
     .collection('Events')
     .where('date', '>=', Timestamp.fromDate(currentDate))
     .orderBy('date', 'asc')
-    .limit(limit)
     .get()
     .then((snapshot) => {
       return snapshot.docs.map((doc) => {
@@ -28,22 +26,25 @@ export async function getFutureEvents({
 import events from '@/app/events/events.json'
 
 export async function getPastEvents({
-  limit = 1000,
   currentDate = new Date(),
 }: {limit?: number; currentDate?: Date} = {}) {
-  return events
-  //   await new Promise((resolve) => setTimeout(resolve, 1000))
-  //   return firestore
-  //     .collection('Events')
-  //     .where('date', '<', Timestamp.fromDate(currentDate))
-  //     .orderBy('date', 'desc')
-  //     .limit(limit)
-  //     .get()
-  //     .then((snapshot) => {
-  //       return snapshot.docs.map((doc) => {
-  //         return {id: doc.id, ...doc.data()} as Event & {id: string}
-  //       })
-  //     })
+  const firestoreEvents = await firestore
+    .collection('Events')
+    .where('date', '<', Timestamp.fromDate(currentDate))
+    .orderBy('date', 'desc')
+    .get()
+    .then((snapshot) => {
+      return snapshot.docs.map((doc) => {
+        const eventData = doc.data()
+
+        return {
+          id: doc.id,
+          ...eventData,
+          date: eventData.date.toDate().toLocaleDateString('en-CA'),
+        } as Event & {id: string}
+      })
+    })
+  return [...firestoreEvents, ...events]
 }
 
 export async function getEvent(eventId: string) {
