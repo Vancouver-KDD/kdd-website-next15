f90a5569047c120edd3f12b1590dbe8baea75933
Eric Kim <bosung90@gmail.com>
2025-09-12 20:30:16 -0700

    log user activity

--- firebase/server.ts
+++ firebase/server.ts
@@ -2,7 +2,7 @@ import 'server-only'
 
 import {getApp, initializeApp} from 'firebase-admin/app'
 import {getAuth} from 'firebase-admin/auth'
-import {getFirestore} from 'firebase-admin/firestore'
+import {getFirestore, FieldValue} from 'firebase-admin/firestore'
 import {getStorage} from 'firebase-admin/storage'
 
 let app
@@ -27,7 +27,27 @@ export const storage = getStorage(app)
 export async function verifyAdminToken(token: string) {
   const decodedToken = await auth.verifyIdToken(token, true)
   if (!decodedToken.admin) {
-    return {valid: false, message: 'Unauthorized'}
+    return {valid: false, message: 'Unauthorized', userId: decodedToken.uid}
   }
-  return {valid: true, message: 'Authorized'}
+  return {valid: true, message: 'Authorized', userId: decodedToken.uid}
+}
+
+export type LOG_EVENT_TYPE =
+  | 'create_event'
+  | 'update_event'
+  | 'delete_event'
+  | 'move_event_photo'
+  | 'delete_event_photo'
+  | 'add_event_photo'
+  | 'verify_admin_password'
+  | 'step_down_as_admin'
+
+export async function logUserActivity(uid: string, event: LOG_EVENT_TYPE, data: any) {
+  const userInfo = await auth.getUser(uid)
+  await firestore.collection('Logs').add({
+    event,
+    userInfo: userInfo.toJSON(),
+    data,
+    createdAt: FieldValue.serverTimestamp(),
+  })
 }
